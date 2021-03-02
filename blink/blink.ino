883e1c1e3686ffa7b0e7ab9e7e3162b232e094bd
dmitry.nasyrkulov@gmail.com <dmitry.nasyrkulov@gmail.com>
2021-03-02 22:54:33 +0200

    interrupt demo ашч

--- blink/blink.ino
+++ blink/blink.ino
@@ -1,15 +1,10 @@
 void setup() {
   pinMode(2, OUTPUT);
-  pinMode(4, OUTPUT);
 }
 
 void loop() {
   digitalWrite(2, HIGH);
   delay(5000);
   digitalWrite(2, LOW);
-  delay(5000);
-  digitalWrite(4, HIGH);
-  delay(5000);
-  digitalWrite(4, LOW);
   delay(5000);
 }
--- interrupt/interrupt/interrupt.ino
+++ interrupt/interrupt/interrupt.ino
@@ -1,21 +1,44 @@
-volatile boolean intFlag = false;   // флаг
+// volatile boolean intFlag = false;   // флаг
+
+// void setup() {
+//   Serial.begin(9600); // открыли порт для связи
+//   // подключили кнопку на D2 и GND
+//   pinMode(5, INPUT);
+//   // D2 это прерывание 0
+//   // обработчик - функция sensorRead
+//   // FALLING - при нажатии на кнопку будет сигнал 0, его и ловим
+//   attachInterrupt(0, sensorRead, LOW);
+// }
+// void sensorRead() {
+//   intFlag = true;   // подняли флаг прерывания
+// }
+// void loop() {
+//   if (intFlag) {
+//     intFlag = false;    // сбрасываем
+//     // совершаем какие-то действия
+//     Serial.println("Interrupt!");
+//   }  
+// }
+
+
+
+unsigned long lastflash;
+int RPM;
 
 void setup() {
-  Serial.begin(9600); // открыли порт для связи
-  // подключили кнопку на D2 и GND
-  pinMode(5, INPUT);
-  // D2 это прерывание 0
-  // обработчик - функция sensorRead
-  // FALLING - при нажатии на кнопку будет сигнал 0, его и ловим
-  attachInterrupt(0, sensorRead, LOW);
+  Serial.begin(9600);  //открыть порт
+  attachInterrupt(0,sens,RISING); //подключить прерывание на 2 пин при повышении сигнала
 }
-void sensorRead() {
-  intFlag = true;   // подняли флаг прерывания
+void sens() {
+  RPM=(1000000.0/(float)(micros()-lastflash))*60;  //расчет
+  lastflash=micros();  //запомнить время последнего оборота
 }
+
 void loop() {
-  if (intFlag) {
-    intFlag = false;    // сбрасываем
-    // совершаем какие-то действия
-    Serial.println("Interrupt!");
-  }  
+  if ((micros()-lastflash)>1000000){ //если сигнала нет больше секунды
+    RPM=0;  //считаем что RPM 0
+  }
+   //вывод в порт
+    Serial.println(RPM);
+  delay(50);  //задержка для стабильности
 }
