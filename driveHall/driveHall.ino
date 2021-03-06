6146639e43595d94a9055413f296ceccb4405ca9
dmityfrontender <dmitry.nasyrkulov@gmail.com>
2021-03-06 13:20:07 +0200

    button demo

--- driveHall/driveHall.ino
+++ driveHall/driveHall.ino
@@ -1,19 +1,21 @@
 int val; 
 
 void setup() {
+  Serial.begin(9600);
   pinMode (5, INPUT);  
-  pinMode(2, OUTPUT);
+ // pinMode(2, OUTPUT);
   //pinMode(4, OUTPUT);
 }
 
 void loop() {
-  val = digitalRead (5) ; // читаем сенсор
+  val = analogRead (5) ; // читаем сенсор
   
-  if (val == LOW) {
-    digitalWrite(2, LOW);
+   if (val == LOW) {
+   // digitalWrite(2, LOW);
     //digitalWrite(4, HIGH);
   } else {
+    Serial.println("tyt");
     //digitalWrite(4, LOW);
-    digitalWrite(2, HIGH);
+   // digitalWrite(2, HIGH);
   }
 }
