71243f6ce08683c4f1677dfe7ef7de73c0f427c7
dmitry.nasyrkulov@gmail.com <dmitry.nasyrkulov@gmail.com>
2021-03-11 22:59:50 +0200

    pre alpha

--- arduino-module/train-prog/train-prog.ino
+++ arduino-module/train-prog/train-prog.ino
@@ -8,12 +8,19 @@ GButton myButt4;
 GButton myButt5;
 GButton myButt6;
 
+//LIGHTS
+int lightBlue = 11;
+int lightWhite = 12;
+int lightRed = 13;
+int lightGreen = 14;
+
+int dose = 1650;
+
 Servo servo1;
 Servo servo2;
 
 const byte interruptPin = 2;
 volatile byte state = LOW;
-
 
 int sensorVal1;
 int sensorVal2;
@@ -22,39 +29,61 @@ bool flag = false;
 int iterations = 0;
 int passengers;
 
-
 void manageButton(int num) {
   passengers = num;
+
+  digitalWrite(lightBlue, LOW); //blue light
+  digitalWrite(lightWhite, HIGH); //white light
+  delay(100);
+
   digitalWrite(7, HIGH);
-  
-  for (int i = 0; i <= num; i++) {
-    servo2.write(210);
-    delay(1000);
-    servo2.write(0);
-    delay(1000);
-  }
-  
-  servo1.write(23); 
+  delay(1000);
+  servo1.write(107);
+  delay(1000);
+  servo2.write(210);
   delay(1000);
 }
 
 void managePump () {
   digitalWrite(7, LOW);
 
-  if (!interations) {
-    servo1.write(107);
-    delay(1000);
-  }
+  if (iterations < passengers) {
 
-  iterations++;
+    digitalWrite(4, HIGH);
+    delay(dose);  //1650 children ticket  3700 adult ticket
+    digitalWrite(4, LOW);
+    delay(1000);
+    digitalWrite(5, HIGH);
+    delay(1850);
+    digitalWrite(5, LOW);
+    delay(1000);
+
+    if (iterations == passengers - 1) {
+      servo2.write(0);
+      delay(1000);
+      servo1.write(23); 
+      delay(1000);
+
+      digitalWrite(lightWhite, LOW); //white light
+      delay(100);
+      digitalWrite(lightBlue, HIGH); //blue light
+      delay(100);
+      digitalWrite(lightRed, LOW); //red light
+      delay(100);
+      digitalWrite(lightGreen, HIGH); //green light
+    } 
+    
+    iterations++;
+    digitalWrite(7, HIGH);
+  }
 }
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode (11, OUTPUT);   //blue light
-  pinMode (12, OUTPUT);   //whight light
-  pinMode (13, OUTPUT);   //red light
-  pinMode (14, OUTPUT);   //green light
+  pinMode (lightBlue, OUTPUT);   //blue light
+  pinMode (lightWhite, OUTPUT);   //white light
+  pinMode (lightRed, OUTPUT);   //red light
+  pinMode (lightGreen, OUTPUT);   //green light
   pinMode (2, OUTPUT);  // train moove forvard
   pinMode (3, OUTPUT);  //train moove back
   pinMode (4, OUTPUT);  //pump forvard
@@ -83,12 +112,12 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(11,HIGH); //blue light
-  digitalWrite(13,HIGH); //red light
+  digitalWrite(lightBlue, HIGH); //blue light
+  digitalWrite(lightRed, HIGH); //red light
   
   sensorVal1 = digitalRead(6);
-                                                    // BACK TRAIN TO START POSITION
-  if (!flag) {
+
+  if (!flag) {  // BACK TRAIN TO START POSITION
     digitalWrite(3, HIGH);
     flag = true;
   }
@@ -96,16 +125,19 @@ void loop() {
   if (sensorVal1 == LOW) {  // читаем значение сенсора
    digitalWrite(3, LOW);
   }
-  int analog = analogRead(A1);
+  int analogBtns = analogRead(A1);
+  int analogDose = analogRead(A3);
+  if (analogDose > 700) {
+    dose = 3700;
+  }
   // для начала нужно вывести и запомнить значение для каждой кнопки
-//  Serial.println(analog);
   // проверяем у каждой кнопки свой диапазон (+- 20 от полученного значения)
-  myButt1.tick(analog < 775 && analog > 735);
-  myButt2.tick(analog < 595 && analog > 555);
-  myButt3.tick(analog < 460 && analog > 390);
-  myButt4.tick(analog < 350 && analog > 310);
-  myButt5.tick(analog < 245 && analog > 200);
-  myButt6.tick(analog < 140 && analog > 100);
+  myButt1.tick(analogBtns < 775 && analogBtns > 735);
+  myButt2.tick(analogBtns < 595 && analogBtns > 555);
+  myButt3.tick(analogBtns < 460 && analogBtns > 390);
+  myButt4.tick(analogBtns < 350 && analogBtns > 310);
+  myButt5.tick(analogBtns < 245 && analogBtns > 200);
+  myButt6.tick(analogBtns < 140 && analogBtns > 100);
 
   if (myButt1.isHolded()) {
     manageButton(1);
